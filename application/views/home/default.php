26798eb19b1bf75b75d55ef9b2641dd8502157e2
Andrew Fulton <andrew@bundle.com.au>
2013-03-18 15:54:18 +1100

    updating support links

--- application/views/home/default.php
+++ application/views/home/default.php
@@ -3,7 +3,7 @@
         <p>This May, a metaphorical Winnebago containing 7 Australian comics creators will embark upon a 3 week North American tour. The Caravan will be visit numerous festivals and comics retailers throughout the USA and Canada, showcasing some of the best comics made in this country. It will be a professional development opportunity for them, allowing them to network with their overseas peers and to engage in some shameless huckstering. </p>
         <p>The Caravaneers are: Scarlette Baccini, Mirranda Burton, Marijka Gooding, Patrick Alexander, Gregory Mackay, Dan Hayward and Bruce Mutard. </p>
         <p>In addition, the Caravan will be screening a feature length documentary - <strong>Graphic Novels! Melbourne!</strong> (Aisle 5 films) - which illuminates how Melbourne’s strange water produces so many good comics. The director/producer/DP of the film, Dan Hayward, will be accompanying the tour to present his film at a number of screenings.</p>
-        <p><a href="/support.html" class="get-on-board">Get on Board.</a></p>
+        <p><a href="<?php print URL::to_route('support'); ?>" class="get-on-board">Get on Board.</a></p>
         <ul id="contact">
             <li><a title="Like Caravan of Comics on Facebook" class="contact-facebook" href="http://facebook.com/caravanofcomics">Facebook</a></li>
             <li><a title="Follow Caravan of Comics on Twitter" class="contact-twitter" href="http://twitter.com/caravanofcomics">Twitter</a></li>
--- application/views/support/default.php
+++ application/views/support/default.php
@@ -4,6 +4,8 @@
     <div id="content-about">
         <h2>How do I get aboard the Caravan?</h2>
 
+        <iframe style="float:left;" src="http://www.indiegogo.com/project/358899/widget" width="224px" height="486px" frameborder="0" scrolling="no"></iframe>
+
         <p>Whilst financing for this trip has been made possible by a generous grant from the literature board of the Australia Council of Arts, this covers less than one third of actual costs. We are running an Indiegogo campaign to help fund the rest, and there are a plethora of rewards on offer for pledges of anywhere between $5 and $1200.</p>
         <p>You can also follow us on <a href="http://facebook.com/caravanofcomics" title="Caravan of comics on Facebook">Facebook</a><a> and </a><a href="http://twitter.com/caravanofcomics" title="Follow Caravan of Comics on Twitter">Twitter</a> - and tell two friends to do the same. And tell them to tell two of their friends. And so on.</p>
 
